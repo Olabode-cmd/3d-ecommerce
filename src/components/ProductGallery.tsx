1807c5f67b4cd3709245a1b3a1cc4f0c27ce436f
Olabode-cmd <olabodebalogun80@gmail.com>
2025-11-06 18:43:06 +0100

    cleanup

--- src/components/ProductGallery.tsx
+++ src/components/ProductGallery.tsx
@@ -40,30 +40,7 @@ export default function ProductGallery({ product }: ProductGalleryProps) {
         <ThreeDViewer product={product} compact />
         <ARViewer product={product} compact />
       </div>
-      {/* <div className="flex flex-col md:flex-row md:space-x-3 space-y-3 md:space-y-0">
-        <div className="flex space-x-3 md:contents">
-          {product.images.map((image, index) => (
-            <button
-              key={index}
-              onClick={() => setSelectedImage(index)}
-              className={`flex-1 md:w-20 md:flex-none h-20 rounded-lg overflow-hidden border-2 transition-colors ${
-                selectedImage === index ? 'border-blue-500' : 'border-gray-200 hover:border-gray-300'
-              }`}
-            >
-              <img 
-                src={image} 
-                alt={`${product.name} ${index + 1}`}
-                className="w-full h-full object-cover"
-              />
-            </button>
-          ))}
-        </div>
-        
-        <div className="flex space-x-2 md:contents">
-          <ThreeDViewer product={product} compact />
-          <ARViewer product={product} compact />
-        </div>
-      </div> */}
+      
     </div>
   );
 }
